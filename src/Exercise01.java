31ec3c1f4bfc0571dd14514bf000ab43f3d20b9c
michaeldelli-gatti <michaelddg@gmail.com>
2016-05-23 10:22:58 -0400

    Exercise 1

---
+++ src/Exercise01.java
@@ -0,0 +1,44 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ * Created by michaeldelli-gatti on 5/23/16.
+ */
+public class Exercise01 {
+    public static void main(String[] args) {
+        String[] names = { "Alice", "Bob", "Charlie","David", "Andrea", "Albert", "Beth"};
+
+        HashMap<String, ArrayList<String>> nameMap = new HashMap<>();
+
+        for(String name : names){
+            char firstLetter = name.charAt(0);
+            String firstLetterStr = String.valueOf(firstLetter);
+            nameMap.put(firstLetterStr, new ArrayList<>());
+        }
+
+        for (String name : names){
+            char firstLetter = name.charAt(0);
+            String firstLetterStr = String.valueOf(firstLetter);
+            ArrayList<String> arr = nameMap.get(firstLetterStr);
+            arr.add(name);
+        }
+        System.out.println(nameMap);
+
+        //alt solution:
+        nameMap = new HashMap<>();
+        for (String name : names){
+            char firstLetter = name.charAt(0);
+            String firstLetterStr = String.valueOf(firstLetter);
+            ArrayList<String> arr = nameMap.get(firstLetterStr);
+            if (arr == null) {
+                arr = new ArrayList<>();
+                arr.add(name);
+                nameMap.put(firstLetterStr, arr);
+            }
+            else{
+                arr.add(name);
+            }
+        }
+        System.out.println(nameMap);
+    }
+}
